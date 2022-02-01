e022c6ce65a735a083e53b5803c70768c3fae64d
IonutCatalin <ionutcatalin2898@yahoo.com>
2022-02-01 23:13:57 +0200

    adding products

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBwywKJYUhj5LcegBmvX8_csbzIaPoCxdc",
+  authDomain: "eccomerce-2cc3f.firebaseapp.com",
+  projectId: "eccomerce-2cc3f",
+  storageBucket: "eccomerce-2cc3f.appspot.com",
+  messagingSenderId: "324941666175",
+  appId: "1:324941666175:web:569a2858ce695ef23c6c4a",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
